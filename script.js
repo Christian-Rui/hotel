3ecaa728bb56087053f8376123d6ccb5831b9c6c
Christian-Rui <113655013+Christian-Rui@users.noreply.github.com>
2024-04-04 17:56:33 -0300

    Adicionando mais informações

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ function VerificarSenha(){
     if(parseInt(senhadigitada) === senha){
         ApresentarNaTela("Senha válida!");
         ApresentarNaTela(`Bem vindo ao hotel ${nomeHotel}, ${nomeUsuario}.\nÉ um imenso prazer ter você por aqui!`);
+        Inicio();
+
     }  
     else if(senhadigitada.trim() === ""){
         ApresentarNaTela("Nenhum senha foi inserida... Tente novamente");
@@ -21,20 +23,35 @@ function VerificarSenha(){
     }
 }
 
-// function Inicio(){
-//     switch(){
-//         case :
-//             break;
-//         default;
-//     }
-// }
+function EscolherQuarto(){
+    var quantidadeQuartos = 20;
+    var valorDiaria = parseFloat(prompt("Digite o valor da diária desejada:"));
+    var quantidadeDias = parseInt(prompt("Digite a quantidade de dias que deseja se hospedar:"));
+}
 
-ApresentarNaTela(`Muito obrigado e até logo, ${nomeUsuario}.`);
+function ReceberValor(texto){
+    return prompt()
+}
 
-// function Erro(){
 
-// }
+function Inicio(){
+    var escolhaOpcao = parseInt(prompt("Digite qual opção deseja: \n\n[ 1 ] - Escolher Quarto \n\n[ 6 ] - Sair do hotel"))
+    switch(escolhaOpcao){
+        case 1 :
+            EscolherQuarto();
+            break;
+        case 6:
+            ApresentarNaTela(`Muito obrigado e até logo, ${nomeUsuario}.`);
+            break;
+        default:
+            Erro(escolhaOpcao);
+    }
+}
 
+function Erro(numero){
+    ApresentarNaTela(`O número ${numero} não corresponde a uma opção válida! Tente novamente.`);
+    Inicio();
+}
 
 function ApresentarNaTela(conteudo){
     alert(conteudo);
