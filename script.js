afd72d769003c7c545f79936ac35a8b54af70aed
Christian Rui <chris2w0social@gmail.com>
2024-04-17 13:53:28 -0300

    Adicionando todas as funções, necessita de refatorar

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function VerificarSenha() {
 }
 
 function Inicio() {
-    var escolhaOpcao = parseInt(prompt("Digite qual opção deseja: \n\n[ 1 ] - Reservar Quarto \n\n[ 2 ] - Cadastrar Hospedes \n\n[ 3 ] - Cadastrar e Pesquisar\n\n[ 6 ] - Sair do hotel"))
+    var escolhaOpcao = parseInt(prompt("Digite qual opção deseja: \n\n[ 1 ] - Reservar Quarto \n\n[ 2 ] - Cadastrar Hospedes \n\n[ 3 ] - Cadastrar e Pesquisar \n\n[ 4 ] - Eventos\n\n [ 5 ] - Passeios\n\n [ 6 ] - Manutenção\n\n[ 7 ] - Sair do hotel"))
     switch (escolhaOpcao) {
         case 1:
             ReservarQuarto();
@@ -41,8 +41,17 @@ function Inicio() {
         case 3:
             CadastroEPesquisa();
             break;
+        case 4:
+            Eventos();
+            break;
+        case 5:
+            Passeios();
+            break;
         case 6:
-            confirm('Deseja sair do hotel: ') ? Inicio() : CadastroEPesquisa();
+            Manuntencao();
+            break;
+        case 7:
+            Sair('Deseja sair do hotel: ')
             break;
         default:
             Erro(escolhaOpcao);
@@ -108,6 +117,20 @@ function VerificarQuartoDisponivel(texto, valorMaximo, quartos) {
     }
     return numeroQuarto;
 
+    // var quantidadeMaximaDeQuartos = 20;
+    // var quarto = prompt('Digite o número do quarto que deseja (de 1 a 20): ');
+    // var listaQuartosReservados = []
+    // if (listaQuartosReservados.length > 0) {
+    //     if (listaCadastroHospedes.includes(quarto)) {
+    //         alert('Quarto já está ocupado');
+    //     } else if (quarto < 0 || quarto > quantidadeMaximaDeQuartos) {
+    //         alert('Valor inválido');
+    //     } else {
+    //         listaQuartosReservados.push(quarto);
+    //     }
+    // } else {
+    //     listaQuartosReservados.push(quarto);
+    // }
 }
 
 function RetornarSenha(textoPrompt) {
@@ -124,7 +147,7 @@ function RetornarSenha(textoPrompt) {
 function RetornarNumeroPositivo(textoPrompt, max) {
     var valor = ReceberValorUsuario(textoPrompt);
 
-    if(valor === null){
+    if (valor === null) {
         Cancelar("Você deseja cancelar operação e voltar para o menu inicial") ? Inicio() : RetornarNumeroPositivo(textoPrompt, max);
     }
 
@@ -258,35 +281,35 @@ function CadastroEPesquisa() {
 
 
 
-function Cadastrar(){
+function Cadastrar() {
     var hospede = ReceberValorUsuario("Qual o nome do hospede: ");
 
-   if(hospede === null){
-    Cancelar("Você deseja cancelar operação e voltar para o menu anterior") ? CadastroEPesquisa() : Cadastrar();
-   }
+    if (hospede === null) {
+        Cancelar("Você deseja cancelar operação e voltar para o menu anterior") ? CadastroEPesquisa() : Cadastrar();
+    }
 
-   if((nomesHospedes.length + 1) < 3) {
-    nomesHospedes.push(hospede.toLowerCase());
-   } else {
-    ApresentarNaTela("Máximo de cadastros atingido");
-   }
-   CadastroEPesquisa()
+    if ((nomesHospedes.length + 1) < 3) {
+        nomesHospedes.push(hospede.toLowerCase());
+    } else {
+        ApresentarNaTela("Máximo de cadastros atingido");
+    }
+    CadastroEPesquisa()
 }
 
-function Pesquisar(){
+function Pesquisar() {
     var hospede = ReceberValorUsuario("Qual o nome do hospede você deseja pesquisar: ");
 
-   if(hospede === null){
-    Cancelar("Você deseja cancelar operação e voltar para o menu anterior") ? CadastroEPesquisa() : Pesquisar();
-   }
+    if (hospede === null) {
+        Cancelar("Você deseja cancelar operação e voltar para o menu anterior") ? CadastroEPesquisa() : Pesquisar();
+    }
 
-   if(nomesHospedes.includes(hospede.toLowerCase())){
-    ApresentarNaTela(`Hospede ${nomesHospedes[nomesHospedes.indexOf(hospede.toLowerCase())]} foi encontrado`);
-   } else {
-    ApresentarNaTela('Hospede não encontrado');
-   }
+    if (nomesHospedes.includes(hospede.toLowerCase())) {
+        ApresentarNaTela(`Hospede ${nomesHospedes[nomesHospedes.indexOf(hospede.toLowerCase())]} foi encontrado`);
+    } else {
+        ApresentarNaTela('Hospede não encontrado');
+    }
 
-   CadastroEPesquisa();
+    CadastroEPesquisa();
 }
 
 function Listar() {
@@ -294,7 +317,191 @@ function Listar() {
     CadastroEPesquisa();
 }
 
-function Cancelar(texto){
+
+
+
+function Eventos() {
+    var laranja = 150;
+    var laranjaAdicional = 70;
+
+    var colorado = 350;
+
+    var convidados = RetornarNumeroPositivo('Qual é o número de convidados para esse evento: \n\nOBS: Numero mínimo de convidade é 1, e o máximo é de 350!', colorado);
+
+    if (convidados === null) {
+        Cancelar("Você deseja cancelar operação e voltar para o menu inicial") ? Inicio() : Eventos();
+    }
+
+    var adicionaisNecessarios = 0;
+    var textoAuditorio = '';
+    if (convidados < (laranja + laranjaAdicional)) {
+        textoAuditorio = 'Use o auditório Laranja'
+        if (convidados > laranja) {
+            adicionaisNecessarios = convidados - laranja;
+            textoAuditorio += ` inclua mais ${adicionaisNecessarios} cadeiras)`
+        }
+    } else {
+        textoAuditorio = 'Use o auditório Colorado';
+    }
+    ApresentarNaTela(textoAuditorio)
+    ApresentarNaTela('Agora vamos ver a agenda do evento');
+
+    var reservaMinima = 7;
+    var reservaMaximaSemana = 23;
+    var reservaMaximaFimDeSemana = 15;
+
+    var diaSemana = ReceberValorUsuario('Qual o dia da semana que deseja reservar: ');
+
+    var semana = ['segunda', 'terça', 'quarta', 'quinta', 'sexto'];
+    var fimSemana = ['sabado', 'domingo'];
+    if (diaSemana === null) {
+        Cancelar("Você deseja cancelar operação e voltar para o menu inicial") ? Inicio() : Eventos();
+    }
+
+    var horario = parseInt(RetornarNumeroPositivo('Qual o horário deseja reservar: '));
+
+    var textoReserva = '';
+    if (semana.includes(diaSemana) && horario >= reservaMinima && horario <= reservaMaximaSemana || fimSemana.includes(diaSemana) && horario >= reservaMinima && horario <= reservaMaximaFimDeSemana) {
+
+        textoReserva = 'Auditório disponível';
+        ApresentarNaTela(textoReserva);
+    } else {
+        textoReserva = 'Auditório indisponível';
+        ApresentarNaTela(textoReserva);
+        Eventos();
+    }
+
+    var nomeEmpresa = ReceberValorUsuario('Qual o nome da empresa: ');
+
+    if (nomeEmpresa === null) {
+        Cancelar("Você deseja cancelar operação e voltar para o menu inicial") ? Inicio() : Eventos();
+    }
+
+    ApresentarNaTela(`Auditório reservado para ${nomeEmpresa}: ${diaSemana} às ${horario}hs`);
+
+    var garcomPagamento = 10.50;
+
+    var duracao = RetornarNumeroPositivo("Qual a duração do evento em horas? ");
+
+    var garcomPorConvidado = Math.ceil(convidados / 12);
+
+    var garcomPorHora = Math.ceil(duracao / 2);
+
+    var custo = parseFloat((garcomPagamento * duracao) * (garcomPorConvidado + garcomPorHora));
+
+    ApresentarNaTela(`São necessários ${garcomPorConvidado + garcomPorHora} garçons`);
+    ApresentarNaTela(`Custo: R$${custo.toFixed(2)}`);
+    ApresentarNaTela('Agora vamos calcular o custo do buffet do hotel para o Eventos.')
+
+
+    var cafePorPessoa = 0.2;
+    var aguaPorPessoa = 0.5;
+    var salgadosPorPessoa = 7;
+
+    var precoCafe = 0.80;
+    var precoAgua = 0.40;
+    var precoSalgado = 34;
+
+    var quantidadeBuffet = {};
+
+    quantidadeBuffet.cafe = cafePorPessoa * convidados;
+    quantidadeBuffet.agua = aguaPorPessoa * convidados;
+    quantidadeBuffet.salgados = salgadosPorPessoa * convidados;
+
+    var valorBuffet = {};
+
+    valorBuffet.cafe = Math.ceil(quantidadeBuffet.cafe) * precoCafe;
+    valorBuffet.agua = Math.ceil(quantidadeBuffet.agua) * precoAgua;
+    valorBuffet.salgados = Math.ceil(quantidadeBuffet.salgados / 100) * precoSalgado;
+
+    ApresentarNaTela(`O evento precisará de ${quantidadeBuffet.cafe} litros de café, ${quantidadeBuffet.agua} litros de água, ${quantidadeBuffet.salgados} salgados`);
+
+    var aceitoCustos = confirm(`O café irá custar R$${valorBuffet.cafe}, a água R$${valorBuffet.agua}, e os salgados R$${valorBuffet.salgados}.\nNo total será R$${valorBuffet.cafe + valorBuffet.agua + valorBuffet.salgados}`);
+
+    if (aceitoCustos) {
+        var podeReservar = VerificarTextoRecebido(ReceberValorUsuario('Gostaria de efetuar a reserva? S/N'), ['s', 'n']);
+        if (podeReservar) {
+            ApresentarNaTela(`${nomeUsuario}, reserva efetuada com sucesso.`);
+            Inicio();
+        } else {
+            ApresentarNaTela(('Reserva não efetuada.'));
+            Eventos();
+        }
+    } else {
+        Eventos();
+    }
+}
+
+function Passeios() {
+    var wayne = {};
+    var stark = {};
+    wayne.nome = 'Wayne Oil';
+    wayne.alcool = RetornarNumeroPositivo('Qual o valor do álcool no posto Wayne Oil? ');
+    wayne.gasolina = RetornarNumeroPositivo('Qual o valor da gasolina no posto Wayne Oil? ');
+
+    stark.nome = 'StarkPetrol'
+    stark.alcool = RetornarNumeroPositivo('Qual o valor do álcool no posto Stark Petrol? ');
+    stark.gasolina = RetornarNumeroPositivo('Qual o valor da gasolina no posto Stark Petrol? ');
+
+    var alcoolBarato = wayne.alcool < stark.alcool ? wayne : stark;
+    var gasolinaBarata = wayne.gasolina < stark.gasolina ? wayne : stark;
+
+    var porcentagem = (alcoolBarato.alcool / gasolinaBarata.gasolina) * 100;
+
+    if (porcentagem <= 30) {
+        ApresentarNaTela(`${nomeUsuario}, é mais barato abastecer com alcool no posto ${alcoolBarato.nome}\nO valor final será de R$${alcoolBarato.alcool * 42}`);
+    } else {
+        ApresentarNaTela(`${nomeUsuario}, é mais barato abastecer com gasolina no posto ${gasolinaBarata.nome}\nO valor final será de R$${gasolinaBarata.gasolina * 42}`);
+    }
+
+    Inicio();
+
+}
+
+function Manuntencao() {
+    var listaEmpresas = [];
+    do {
+        const empresas = {}
+
+        empresas.nome = ReceberValorUsuario('Qual o nome da empresa?')
+        var valorServico = RetornarNumeroPositivo('Qual o valor por aparelho?');
+        var quantidadeAparelhos = RetornarNumeroPositivo('Qual a quantidade de aparelhos?');
+        var desconto = ReceberValorUsuario('Qual a porcentagem de desconto?');
+        var quantidadeMin = RetornarNumeroPositivo('Qual o número mínimo de aparelhos para conseguir o desconto?');
+
+        var valorTotal = valorServico * quantidadeAparelhos;
+        console.log(valorTotal);
+        var valorDescontoTotal = 0;
+        console.log(valorDescontoTotal);
+        if (quantidadeAparelhos >= quantidadeMin) {
+            valorDescontoTotal = desconto == 0 ? valorTotal : valorTotal * (1 - desconto / 100);
+            console.log(valorDescontoTotal);
+            ApresentarNaTela(`O serviço de ${empresas.nome} custará R$${valorDescontoTotal.toFixed(2)}`);
+            console.log(valorTotal);
+            empresas.valorFinal = valorDescontoTotal;
+        } else {
+            ApresentarNaTela(`O serviço de ${empresas.nome} custará R$${valorTotal}`);
+            empresas.valorFinal = valorTotal;
+        }
+        listaEmpresas.push(empresas);
+    } while (prompt(`Deseja informar novos dados, ${nomeUsuario}`) === 'n');
+
+    var menorValor = listaEmpresas.reduce((menor, objeto) => {
+        return objeto.valorFinal < menor.valorFinal ? objeto : menor;
+    });
+
+    ApresentarNaTela(`O orçamento de menor vlaor é o ${menorValor.nome} por R$${menorValor.valorFinal}`);
+}
+
+
+
+
+
+
+
+
+
+function Cancelar(texto) {
     return confirm(texto);
 }
 
